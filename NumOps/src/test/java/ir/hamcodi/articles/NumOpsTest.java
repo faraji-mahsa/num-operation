92f1c1e01de0bf36745d9891b86693eb43389c1b
mahsa <mahsa@mahsa>
2014-05-03 17:26:04 +0430

    change the code

---
+++ NumOps/src/test/java/ir/hamcodi/articles/NumOpsTest.java
@@ -0,0 +1,29 @@
+package ir.hamcodi.articles;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ *  * Unit test for simple App.
+ *   */
+public class NumOpsTest extends TestCase
+{
+    /**
+ *      * Create the test case
+ *           *
+ *                * @param testName name of the test case
+ *                     */
+    public NumOpsTest( String testName )
+    {
+        super( testName );
+    }
+
+     public void testNumOps()
+    {
+    testNumOps nops = new testNumOps();
+         assertTrue(nops.size() == 1);
+         assertTrue(nops.getOp(0).getDesc().equals("plus"));
+        assertTrue(nops.getOp(0).op(2,1) == 3);
+                    
+    }
+}
